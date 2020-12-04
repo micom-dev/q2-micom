81c91b23f5cbffd7ceef21067e4708ec6b5abe15
Christian Diener <ch.diener@gmail.com>
2020-12-04 13:43:28 -0800

    Bump version: 0.10.0 → 0.10.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md') as f:
 
 setup(
     name="q2-micom",
-    version="0.10.0",
+    version="0.10.1",
     packages=find_packages(),
     package_data={"q2_micom": ["citations.bib", "assets/templates/*.html"]},
     author="Christian Diener",
