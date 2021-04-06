ad7286f9dd4bb428a1a88c31c882c5092d75a3d1
Christian Diener <ch.diener@gmail.com>
2021-04-06 10:41:02 -0700

    Bump version: 0.10.1 → 0.11.0

--- q2_micom/__init__.py
+++ q2_micom/__init__.py
@@ -15,7 +15,7 @@ from q2_micom._viz import (
     fit_phenotype,
 )
 
-__version__ = "0.10.1"
+__version__ = "0.11.0"
 
 
 def read_results(path):
--- setup.py
+++ setup.py
@@ -4,4 +4,4 @@ This is a shim left here for compatibility reasons.
 """
 from setuptools import setup
 
-setup(version="0.10.1")
+setup(version="0.11.0")
